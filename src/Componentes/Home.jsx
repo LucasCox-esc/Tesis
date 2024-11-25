06dfe67fea15316c4426946d35aea5a4b1d85acc
Srcox100 <lcox.esc@gmail.com>
2024-11-25 10:50:22 -0500

    avances

--- src/Componentes/Home.jsx
+++ src/Componentes/Home.jsx
@@ -1,30 +1,50 @@
 import React, { useState } from "react";
 import DynamicTable from "./DynamicTable";
 import "../Styles/Home.css";
+import SidebarMenu from "./SidebarMenu";
+import BackgroundRectangle from "./BackgroundRectangle";
 
 export const Home = () => {
-  const [tasks, setTasks] = useState([]);
-
-  const addPage = () => {
-    const newTask = {
-      id: Date.now(),
-      name: "",
-      etiquetas: "",
-      estado: "Sin empezar",
-      personas: "",
-      progreso: 0,
-      subtasks: [],
+    const [tasks, setTasks] = useState([]);
+    const [pageTitle, setPageTitle] = useState('Gestión de Tareas');
+    const addPage = () => {
+        const newTask = {
+            id: Date.now(),
+            name: "",
+            etiquetas: "",
+            estado: "Sin empezar",
+            personas: "",
+            progreso: 0,
+            subtasks: [],
+        };
+        setTasks([...tasks, newTask]);
     };
-    setTasks([...tasks, newTask]);
-  };
 
-  return (
-    <div className="container">
-      <div className="header">Ejemplo 02</div>
-      <DynamicTable tasks={tasks} setTasks={setTasks} />
-      
-    </div>
-  );
+    return (
+        <div>
+            <div style={{ display: "flex" }}>
+                <div>
+                    <SidebarMenu />
+                </div>
+                <div style={{ width: "100%" }}>
+                    <div style={{ margin: "15px" }}>
+                        <BackgroundRectangle />
+                    </div>
+
+                    <div style={{ margin: "55px" }}>
+                        <input
+                            value={pageTitle}
+                            onChange={(e) => setPageTitle(e.target.value)}
+                            className="custom-input-text"
+                            type="text"
+                            placeholder={pageTitle === '' ? 'Añada su Título' : ''}
+                        />
+                        <DynamicTable tasks={tasks} setTasks={setTasks} />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Home;
